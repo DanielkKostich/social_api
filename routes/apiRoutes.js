df203abcee20271af4e14b310c3680e988069430
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-07 14:27:43 -0500

    added routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../controllers/userController');
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../controllers/thoughtController');
+
+// User routes
+router.route('/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+router.route('/users/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router.route('/users/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+// Thought routes
+router.route('/thoughts')
+  .get(getAllThoughts)
+  .post(createThought);
+
+router.route('/thoughts/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/thoughts/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(removeReaction);
+
+module.exports = router;
