2128ab0b107566a1e2a1951fa084afbb02e18e80
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-08 13:04:21 -0500

    added date format

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/User');
 const Thought = require('../models/Thought');
 
 
@@ -7,8 +7,10 @@ const thoughtController = {
   async getAllThoughts(req, res) {
     try {
       const thoughts = await Thought.find()
+      
       res.json(thoughts);
     } catch (err) {
+      console.log(err);
       res.status(500).json(err);
     }
   },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Thought = require('../models/Thought');
 
 const userController = {
   async getAllUsers(req, res) {
